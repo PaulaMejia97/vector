fbbb2cb87c2e77b055933ebf4ea7ff9b6641ae88
Paula Pardo <paulapardo9706@gmail.com>
2023-11-21 21:46:44 -0500

    vector y ciclo for y while

--- Vector/src/vector.java
+++ Vector/src/vector.java
@@ -1,4 +1,18 @@
 public class vector {
 
+  public static void main(String[] args) {
+    int[] numeros = new int[10];
+    for(int contador=1;contador<=10;contador=contador+1){
+      numeros[contador-1]=contador;
+    }
+    int contador;
+    contador=1;
+
+    while (contador<=10){
+
+      System.out.println(numeros[contador-1]);
+      contador=contador+1;
+    }
+  }
 
 }
